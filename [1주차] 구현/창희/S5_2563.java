087520a1758fe84fa6a0105ceb45410061a777da
ChangHee98 <dlackdgml2@naver.com>
2024-06-27 09:16:57 +0900

    [baekjoon] S5:2563 implementation

--- [1주차] 구현/창희/S5_2563.java
+++ [1주차] 구현/창희/S5_2563.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class confetti_2563 {
+public class S5_2563 {
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[][] canvas = new int[100][100];
