4095d09727a0f74f230b049e3c23033939c16891
ChangHee98 <dlackdgml2@naver.com>
2024-08-01 08:54:32 +0900

    [backjoon] S3:15652 백트래킹

---
+++ [9회차] 백트래킹/창희/S3_15652.java
@@ -0,0 +1,37 @@
+package day09;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S3_15652 {
+    static int n, m;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        sb = new StringBuilder();
+        int[] result = new int[m];
+        backtrack(result, 0, 0);
+        System.out.println(sb);
+    }
+    public static void backtrack(int[] result, int depth, int s){
+        if(depth == m){
+            for(int i : result){
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i=s; i<n; i++){
+            result[depth] = i+1;
+
+            backtrack(result, depth+1, i);
+
+        }
+    }
+
+}
