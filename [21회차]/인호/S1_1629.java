a9bd199bfecd1e090ce5f0f3e2dc3578726ccf17
MeangSung <aoddlsgh98@gmail.com>
2024-11-05 14:15:15 +0900

    fix

--- [21회차]/인호/S1_1629.java
+++ [21회차]/인호/S1_1629.java
@@ -6,17 +6,17 @@ public class S1_1629 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
+        long a = sc.nextInt();
+        long b = sc.nextInt();
+        long c = sc.nextInt();
 
         System.out.println(sol(a,b,c));
     }
 
-    private static int sol(int a, int b, int c) {
+    private static long sol(long a, long b, long c) {
         if(b == 1) return a % c;
 
-        int val = sol(a,b/2,c);
+        long val = sol(a,b/2,c);
 
         val = val * val % c;
         if(b % 2 == 0) return val;
